5600b8820d67192eac8ae0c8bfd42f756b3cffe8
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2024-12-16 21:30:32 +0700

    fix: remove token when logout

--- api/public/auth/logout.go
+++ api/public/auth/logout.go
@@ -8,6 +8,6 @@ import (
 )
 
 func Logout(c *gin.Context) {
-	c.SetCookie("auth_token", "", 0, "/", os.Getenv("GLOBAL_DOMAIN"), false, true)
+	c.SetCookie("auth_token", "", -1, "/", os.Getenv("GLOBAL_DOMAIN"), false, true)
 	c.JSON(http.StatusOK, gin.H{"message": "Logout succesful"})
 }
