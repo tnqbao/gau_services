64b19b3c4a0aa0c87fa1784b8e48adcdb90cf1f7
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2024-12-20 01:32:48 +0700

    add verified status fieid in user_information packacge

--- models/userInformation.go
+++ models/userInformation.go
@@ -3,9 +3,11 @@ package models
 import "time"
 
 type UserInformation struct {
-	UserId      uint       `gorm:"primaryKey" json:"user_id"`
-	FullName    *string    `json:"fullname"`
-	Phone       *string    `gorm:"unique" json:"phone"`
-	Email       *string    `gorm:"unique" json:"email"`
-	DateOfBirth *time.Time `json:"date_of_birth"`
+	UserId          uint       `gorm:"primaryKey" json:"user_id"`
+	FullName        *string    `json:"fullname"`
+	Phone           *string    `gorm:"unique" json:"phone"`
+	IsPhoneVerified bool       `gorm:"default:false" json:"is_phone_verified"`
+	Email           *string    `gorm:"unique" json:"email"`
+	IsEmailVerified bool       `gorm:"default:false" json:"is_email_verified"`
+	DateOfBirth     *time.Time `json:"date_of_birth"`
 }
